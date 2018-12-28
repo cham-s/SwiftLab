5aa0356859fc2eb49f08eaec7f22aa0f9bddf76e
cham-s <cc.attou@gmail.com>
2018-12-28 20:20:31 +0100

    started value and reference type

---
+++ References and Values.playground/Contents.swift
@@ -0,0 +1,76 @@
+import UIKit
+
+//var str = "Hello, playground"
+//
+//let printGreeting = { print("Hello!") }
+//printGreeting()
+//
+//let copyingGreeting = printGreeting
+//copyingGreeting()
+//
+//func createIncrementer() -> () -> Void {
+//    var counter = 0
+//
+//    return {
+//        counter += 1
+//        print(counter)
+//    }
+//}
+//
+//let incrementer = createIncrementer()
+//incrementer()
+//incrementer()
+//
+//let incrementorCopy = incrementer
+//incrementorCopy()
+//incrementer()
+
+struct Person {
+    var name: String
+    var age: Int
+    var favoriteIceCream: String
+}
+
+let person = Person(name: "Jhon", age: 46, favoriteIceCream: "Vanilla")
+
+final class PersonBox {
+    var person: Person
+    
+    init(person: Person) {
+        self.person = person
+    }
+}
+
+let box = PersonBox(person: person)
+
+final class TestContainer {
+    var box: PersonBox!
+}
+
+let container1 = TestContainer()
+let container2 = TestContainer()
+
+container1.box = box
+container2.box = box
+
+print(container1.box.person.name)
+print(container2.box.person.name)
+
+box.person.name = "Not Jane"
+
+print(container1.box.person.name)
+print(container2.box.person.name)
+
+final class Box<T> {
+    var value: T
+    
+    init(value: T) {
+        self.value = value
+    }
+}
+
+final class TestContainer2 {
+    var box: Box<Person>!
+}
+
+
