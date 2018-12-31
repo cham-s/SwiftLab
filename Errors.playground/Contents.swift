99a798b7a2e7e6cdfe2c785af77348268641dfa1
cham-s <cc.attou@gmail.com>
2018-12-31 22:17:15 +0100

    throw

--- Errors.playground/Contents.swift
+++ Errors.playground/Contents.swift
@@ -2,32 +2,27 @@ import UIKit
 
 
 
-enum PasswordError: Error {
-    case empty
-    case short
-    case obvious(message: String)
-}
+//func encrypt(_ str: String, with password: String) throws -> String {
+//    let encrypted = password + str + password
+//    return String(encrypted.reversed())
+//}
+//
+//
+//
+//do {
+//    let encrypted = try encrypt("Secret!", with: "Jh0n")
+//    print(encrypted)
+//} catch {
+//    print("Encryption failed")
+//}
+//
+//do {
+//    let encrypted = try encrypt("Secret!", with: "Jh0n")
+//    print(encrypted)
+//} catch PasswordError.empty {
+//    print("You must Provide a password")
+//}
 
-func encrypt(_ str: String, with password: String) throws -> String {
-    let encrypted = password + str + password
-    return String(encrypted.reversed())
-}
-
-
-
-do {
-    let encrypted = try encrypt("Secret!", with: "Jh0n")
-    print(encrypted)
-} catch {
-    print("Encryption failed")
-}
-
-do {
-    let encrypted = try encrypt("Secret!", with: "Jh0n")
-    print(encrypted)
-} catch PasswordError.empty {
-    print("You must Provide a password")
-}
 enum PasswordError: Error {
     case empty
     case short
@@ -35,14 +30,26 @@ enum PasswordError: Error {
 }
 
 func functionA() {
-    functionB()
-}
-
-func functionB() {
     do {
-        functionC()
+        try functionB()
     } catch {
         print("Error!")
+    }
+}
+
+//func functionB() {
+//    do {
+//        try functionC()
+//    } catch {
+//        print("Error!")
+//    }
+//}
+
+func functionB() throws {
+    do {
+        try functionC()
+    } catch PasswordError.empty {
+        print("Empty password!")
     }
 }
 
@@ -50,4 +57,35 @@ func functionC() throws {
     throw PasswordError.short
 }
 
+func definitelyWontThrow() {
+    print("Shiny!")
+}
+
+enum Failure: Error {
+    case badNetwork(message: String)
+    case broken
+}
+
+func fetchRemote() throws -> String {
+    throw Failure.badNetwork(message: "Filrewall blocked port.")
+}
+
+func fetchLocal() -> String {
+    return "Taylor"
+}
+
+func fetchUserData(unsing closure: () throws -> String) rethrows {
+    let userData = try closure()
+    print("User data received: \(userData)")
+    
+    do {
+        try fetchUserData(unsing: fetchLocal)
+    } catch Failure.badNetwork(message: let message) {
+        print(message)
+    } catch {
+        print("Fetch error")
+    }
+}
+
+fetchUserData(unsing: fetchRemote)
 
