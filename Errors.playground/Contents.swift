05b0915fc7eb0f82b833e5ad702c178e004cc78c
cham-s <cc.attou@gmail.com>
2019-01-01 20:33:33 +0100

    more errors

--- Errors.playground/Contents.swift
+++ Errors.playground/Contents.swift
@@ -23,19 +23,19 @@ import UIKit
 //    print("You must Provide a password")
 //}
 
-enum PasswordError: Error {
-    case empty
-    case short
-    case obvious
-}
-
-func functionA() {
-    do {
-        try functionB()
-    } catch {
-        print("Error!")
-    }
-}
+//enum PasswordError: Error {
+//    case empty
+//    case short
+//    case obvious
+//}
+//
+//func functionA() {
+//    do {
+//        try functionB()
+//    } catch {
+//        print("Error!")
+//    }
+//}
 
 //func functionB() {
 //    do {
@@ -45,47 +45,62 @@ func functionA() {
 //    }
 //}
 
-func functionB() throws {
-    do {
-        try functionC()
-    } catch PasswordError.empty {
-        print("Empty password!")
-    }
-}
+//func functionB() throws {
+//    do {
+//        try functionC()
+//    } catch PasswordError.empty {
+//        print("Empty password!")
+//    }
+//}
+//
+//func functionC() throws {
+//    throw PasswordError.short
+//}
+//
+//func definitelyWontThrow() {
+//    print("Shiny!")
+//}
+//
+//enum Failure: Error {
+//    case badNetwork(message: String)
+//    case broken
+//}
+//
+//func fetchRemote() throws -> String {
+//    throw Failure.badNetwork(message: "Filrewall blocked port.")
+//}
+//
+//func fetchLocal() -> String {
+//    return "Taylor"
+//}
+//
+//func fetchUserData(unsing closure: () throws -> String) rethrows {
+//    let userData = try closure()
+//    print("User data received: \(userData)")
+//
+//}
+//
+//do {
+//    try fetchUserData(unsing: fetchRemote)
+//} catch Failure.badNetwork(message: let message) {
+//    print(message)
+//} catch {
+//    print("Fetch error")
+//}
 
-func functionC() throws {
-    throw PasswordError.short
-}
 
-func definitelyWontThrow() {
-    print("Shiny!")
-}
-
-enum Failure: Error {
-    case badNetwork(message: String)
-    case broken
-}
-
-func fetchRemote() throws -> String {
-    throw Failure.badNetwork(message: "Filrewall blocked port.")
-}
-
-func fetchLocal() -> String {
-    return "Taylor"
-}
-
-func fetchUserData(unsing closure: () throws -> String) rethrows {
-    let userData = try closure()
-    print("User data received: \(userData)")
+class CustomClass: NSObject {
     
-    do {
-        try fetchUserData(unsing: fetchLocal)
-    } catch Failure.badNetwork(message: let message) {
-        print(message)
-    } catch {
-        print("Fetch error")
-    }
 }
 
-fetchUserData(unsing: fetchRemote)
+func makeObject() -> NSObject {
+    return CustomClass()
+}
+
+func testObject() -> NSObject {
+    
+}
+
+
+
 
